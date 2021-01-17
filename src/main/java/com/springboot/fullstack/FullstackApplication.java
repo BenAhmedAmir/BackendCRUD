b2426a6d130be9f4e8a52ec81e6660a19d6fa204
BenAhmedAmir <dragotn@gmail.com>
2021-01-17 21:29:59 +0100

    backend crud employee

---
+++ src/main/java/com/springboot/fullstack/FullstackApplication.java
@@ -0,0 +1,29 @@
+package com.springboot.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@SpringBootApplication
+
+
+public class FullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackApplication.class, args);
+	}
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}
